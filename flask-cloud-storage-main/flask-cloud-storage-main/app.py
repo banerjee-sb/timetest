76d2521281475104a5dc6b9fc1e6789716a7e59e
banerjee-sb <110657718+banerjee-sb@users.noreply.github.com>
2023-03-02 13:41:15 +0530

    Update app.py

--- flask-cloud-storage-main/flask-cloud-storage-main/app.py
+++ flask-cloud-storage-main/flask-cloud-storage-main/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 # Configure this environment variable via app.yaml
 # CLOUD_STORAGE_BUCKET = os.environ['CLOUD_STORAGE_BUCKET']
-CLOUD_STORAGE_BUCKET = 'project-gwc-374204-urlsigner'
+CLOUD_STORAGE_BUCKET = 'casestudy-2-370804-urlsigner'
 credentials = service_account.Credentials.from_service_account_file("credentials.json")
 
 @app.route('/')
